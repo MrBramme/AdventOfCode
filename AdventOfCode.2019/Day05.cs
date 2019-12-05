5faa8b0cc9d5aa227bbba40bf9c7238436a09102
MrBramme <MrBramme@users.noreply.github.com>
2019-12-05 23:27:09 +0100

    Added day 5 part 1

--- AdventOfCode.2019/Day05.cs
+++ AdventOfCode.2019/Day05.cs
@@ -9,7 +9,88 @@ namespace AdventOfCode._2019
     {
         public override void Run(bool test = false)
         {
-            throw new NotImplementedException();
+            if (test)
+            {
+                Console.WriteLine("No tests here, going for real data");
+            }
+            var input = File.ReadAllText("Data/2019/05.txt");
+            
+            var data = input.Split(',').Select(int.Parse).ToArray();
+
+            Console.WriteLine($"Result Phase 1 : {CalcIntcode(data, 1)}");
+        }
+
+        private int CalcIntcode(int[] data, int inputValue)
+        {
+            var currentStep = 0;
+            var done = false;
+            var latest = int.MinValue;
+            while (!done)
+            {
+                var opCode = GetOpCode(data[currentStep]);
+                var (mode1, mode2, mode3) = GetParamModes(data[currentStep]);
+                var a = 0;
+                var b = 0;
+                if (opCode == OpCode.Add || opCode == OpCode.Multiply)
+                {
+                    a = mode1 == Mode.Position ? data[data[currentStep + 1]] : data[currentStep + 1];
+                    b = mode2 == Mode.Position ? data[data[currentStep + 2]] : data[currentStep + 2];
+                }
+
+                switch (opCode)
+                {
+                    case OpCode.Stop:
+                        done = true;
+                        break;
+                    case OpCode.Add:
+                        data[data[currentStep + 3]] = a + b;
+                        currentStep += 4;
+                        break;
+                    case OpCode.Multiply:
+                        data[data[currentStep + 3]] = a * b;
+                        currentStep += 4;
+                        break;
+                    case OpCode.Input:
+                        data[data[currentStep + 1]] = inputValue;
+                        currentStep += 2;
+                        break;
+                    case OpCode.Output:
+                        latest = data[data[currentStep + 1]];
+                        currentStep += 2;
+                        break;
+                }
+            }
+
+            return latest;
+        }
+
+        private (Mode, Mode, Mode) GetParamModes(int input)
+        {
+            var param1 = input % 1000 > 100 ? Mode.Immediate : Mode.Position;
+            var param2 = input % 10000 > 1000 ? Mode.Immediate : Mode.Position;
+            var param3 = input % 100000 > 10000 ? Mode.Immediate : Mode.Position;
+            return (param1, param2, param3);
+        }
+
+        private OpCode GetOpCode(int input)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/arithmetic-operators#code-try-9
+            return (OpCode)(input % 100);
+        }
+
+        enum OpCode
+        {
+            Add = 1,
+            Multiply = 2,
+            Input = 3,
+            Output = 4,
+            Stop = 99,
+        }
+
+        enum Mode
+        {
+            Position = 0,
+            Immediate = 1
         }
     }
 }
