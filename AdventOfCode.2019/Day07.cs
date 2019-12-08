9eb85c1b447ed73cbf2751e8ab435972cd3fb44a
MrBramme <MrBramme@users.noreply.github.com>
2019-12-08 13:34:02 +0100

    Added day 7

--- AdventOfCode.2019/Day07.cs
+++ AdventOfCode.2019/Day07.cs
@@ -2,42 +2,108 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using AdventOfCode._2019.Helpers;
 using AdventOfCode.Domain;
 
 namespace AdventOfCode._2019
 {
     public class Day07 : Solution
     {
+        // a bit stuck on this one, so learned from https://github.com/alexchro93/AdventOfCode to figure it out.
+
         public override void Run(bool test = false)
         {
-            SolvePhase1(test);
+            Console.WriteLine($"Result phase 1: {SolvePhase1(test)}");
+            Console.WriteLine($"Result phase 2: {SolvePhase2(test)}");
         }
 
-        private void SolvePhase1(bool test)
+        private int SolvePhase1(bool test)
         {
-            var input = GetInput(test);
+            var data = GetData(test);
             var permutations = GetPermutations(new[] { 0, 1, 2, 3, 4 }, 5);
+            var maxOutput = int.MinValue;
+
+            foreach (var permutation in permutations)
+            {
+                var permArr = permutation.ToArray();
+                var output = 0;
+
+                for (var t = 0; t < 5; t++)
+                {
+                    var input = permArr[t];
+                    var computer = new IntCodeComputer(data, input, output);
+                    while (!computer.Complete)
+                        computer.PerformNextInstruction();
+                    output = computer.Output;
+                }
+
+                if (output > maxOutput)
+                {
+                    maxOutput = output;
+                }
+            }
+
+            return maxOutput;
+
         }
 
-        private string[] GetInput(bool test)
+        private int SolvePhase2(bool test)
+        {
+            var data = GetData(test);
+            var permutations = GetPermutations(new[] { 5, 6, 7, 8, 9 }, 5);
+            var maxOutput = int.MinValue;
+
+            foreach (var permutation in permutations)
+            {
+                for (var t = 0; t < 5; t++)
+                {
+                    var computers = permutation.Select(c => new BlockingIntCodeComputer(data, c)).ToList();
+                    computers[0].Input.Add(0);
+                    var tasks = new Task[5];
+
+                    for (var i = 0; i < 5; i++)
+                    {
+                        var c = computers[i];
+                        var cNex = computers[(i + 1) % 5];
+                        c.OutputProduced += o => cNex.Input.Add(o);
+                        var run = Task.Run(() =>
+                        {
+                            while (!c.Complete)
+                                c.PerformNextInstruction();
+                        });
+                        tasks[i] = run;
+                    }
+
+                    Task.WaitAll(tasks);
+
+                    if (computers[4].Output > maxOutput)
+                        maxOutput = computers[4].Output;
+                }
+            }
+
+            return maxOutput;
+
+        }
+
+        private int[] GetData(bool test)
         {
             string data;
             if (test)
             {
-                data = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"; // expected: 43210
+                data = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"; 
             }
             else
             {
                 data = File.ReadAllText("Data/2019/07.txt");
             }
             
-            return data.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            return data.Split(',').Select(int.Parse).ToArray();
         }
 
         // Source: https://stackoverflow.com/a/10630026
         private static IEnumerable<IEnumerable<int>> GetPermutations(IEnumerable<int> list, int length)
         {
-            Console.WriteLine($"Perm with length {length}");
             if (length == 1) return list.Select(t => new int[] { t });
 
             return GetPermutations(list, length - 1)
@@ -45,104 +111,6 @@ namespace AdventOfCode._2019
                     (t1, t2) => t1.Concat(new int[] { t2 }));
         }
 
-        #region IntCode Program of day 6
-        private int CalcIntcode(int[] data, int inputValue)
-        {
-            var currentStep = 0;
-            var done = false;
-            var latest = int.MinValue;
-
-            while (!done)
-            {
-                var opCode = GetOpCode(data[currentStep]);
-                var (mode1, mode2, mode3) = GetParamModes(data[currentStep]);
-                var a = 0;
-                var b = 0;
-
-                if (opCode == OpCode.Add
-                    || opCode == OpCode.Multiply
-                    || opCode == OpCode.JumpIfTrue
-                    || opCode == OpCode.JumpIfFalse
-                    || opCode == OpCode.LessThan
-                    || opCode == OpCode.Equals)
-                {
-                    a = mode1 == Mode.Position ? data[data[currentStep + 1]] : data[currentStep + 1];
-                    b = mode2 == Mode.Position ? data[data[currentStep + 2]] : data[currentStep + 2];
-                }
-                switch (opCode)
-                {
-                    case OpCode.Stop:
-                        done = true;
-                        break;
-                    case OpCode.Add:
-                        data[data[currentStep + 3]] = a + b;
-                        currentStep += 4;
-                        break;
-                    case OpCode.Multiply:
-                        data[data[currentStep + 3]] = a * b;
-                        currentStep += 4;
-                        break;
-                    case OpCode.Input:
-                        data[data[currentStep + 1]] = inputValue;
-                        currentStep += 2;
-                        break;
-                    case OpCode.Output:
-                        latest = data[data[currentStep + 1]];
-                        currentStep += 2;
-                        break;
-                    case OpCode.JumpIfTrue:
-                        currentStep = a == 0 ? currentStep + 3 : b;
-                        break;
-                    case OpCode.JumpIfFalse:
-                        currentStep = a != 0 ? currentStep + 3 : b;
-                        break;
-                    case OpCode.LessThan:
-                        data[data[currentStep + 3]] = a < b ? 1 : 0;
-                        currentStep += 4;
-                        break;
-                    case OpCode.Equals:
-                        data[data[currentStep + 3]] = a == b ? 1 : 0;
-                        currentStep += 4;
-                        break;
-                }
-            }
-
-            return latest;
-        }
-
-        private (Mode, Mode, Mode) GetParamModes(int input)
-        {
-            var param1 = input % 1000 > 100 ? Mode.Immediate : Mode.Position;
-            var param2 = input % 10000 > 1000 ? Mode.Immediate : Mode.Position;
-            var param3 = input % 100000 > 10000 ? Mode.Immediate : Mode.Position;
-            return (param1, param2, param3);
-        }
-
-        private OpCode GetOpCode(int input)
-        {
-            // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/arithmetic-operators#code-try-9
-            return (OpCode)(input % 100);
-        }
-
-        enum OpCode
-        {
-            Add = 1,
-            Multiply = 2,
-            Input = 3,
-            Output = 4,
-            JumpIfTrue = 5,
-            JumpIfFalse = 6,
-            LessThan = 7,
-            Equals = 8,
-            Stop = 99,
-        }
-
-        enum Mode
-        {
-            Position = 0,
-            Immediate = 1
-        }
-
-        #endregion IntCode Program of day 6
+       
     }
 }
---
+++ AdventOfCode.2019/Helpers/Day7/IntCodeComputer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+namespace AdventOfCode._2019.Helpers
+{
+    public class IntCodeComputer
+    {
+        public IntCodeComputer(int[] data, params int[] input)
+        {
+            Data = new int[data.Length];
+            Array.Copy(data, Data, data.Length);
+            Input = input;
+        }
+
+        private int _inpLoc;
+        private bool _complete;
+
+        public int[] Data { get; }
+        public int[] Input { get; }
+        public int Output { get; private set; }
+        public int currentStep { get; private set; }
+        public bool Complete => _complete || currentStep >= Data.Length;
+
+        public void PerformNextInstruction()
+        {
+            if (Complete) return;
+
+            Output = CalcIntcode();
+        }
+
+        private int CalcIntcode()
+        {
+            var latest = int.MinValue;
+
+            while (!_complete)
+            {
+                var opCode = GetOpCode(Data[currentStep]);
+                var (mode1, mode2) = GetParamModes(Data[currentStep]);
+                var a = 0;
+                var b = 0;
+
+                if (opCode == OpCode.Add
+                    || opCode == OpCode.Multiply
+                    || opCode == OpCode.JumpIfTrue
+                    || opCode == OpCode.JumpIfFalse
+                    || opCode == OpCode.LessThan
+                    || opCode == OpCode.Equals)
+                {
+                    a = mode1 == Mode.Position ? Data[Data[currentStep + 1]] : Data[currentStep + 1];
+                    b = mode2 == Mode.Position ? Data[Data[currentStep + 2]] : Data[currentStep + 2];
+                }
+                switch (opCode)
+                {
+                    case OpCode.Stop:
+                        _complete = true;
+                        break;
+                    case OpCode.Add:
+                        Data[Data[currentStep + 3]] = a + b;
+                        currentStep += 4;
+                        break;
+                    case OpCode.Multiply:
+                        Data[Data[currentStep + 3]] = a * b;
+                        currentStep += 4;
+                        break;
+                    case OpCode.Input:
+                        Data[Data[currentStep + 1]] = Input[_inpLoc++];
+                        currentStep += 2;
+                        break;
+                    case OpCode.Output:
+                        latest = Data[Data[currentStep + 1]];
+                        currentStep += 2;
+                        break;
+                    case OpCode.JumpIfTrue:
+                        currentStep = a == 0 ? currentStep + 3 : b;
+                        break;
+                    case OpCode.JumpIfFalse:
+                        currentStep = a != 0 ? currentStep + 3 : b;
+                        break;
+                    case OpCode.LessThan:
+                        Data[Data[currentStep + 3]] = a < b ? 1 : 0;
+                        currentStep += 4;
+                        break;
+                    case OpCode.Equals:
+                        Data[Data[currentStep + 3]] = a == b ? 1 : 0;
+                        currentStep += 4;
+                        break;
+                }
+            }
+
+            return latest;
+        }
+
+        private (Mode, Mode) GetParamModes(int input)
+        {
+            var param1 = input % 1000 > 100 ? Mode.Immediate : Mode.Position;
+            var param2 = input % 10000 > 1000 ? Mode.Immediate : Mode.Position;
+            return (param1, param2);
+        }
+
+        private OpCode GetOpCode(int input)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/arithmetic-operators#code-try-9
+            return (OpCode)(input % 100);
+        }
+
+        enum OpCode
+        {
+            Add = 1,
+            Multiply = 2,
+            Input = 3,
+            Output = 4,
+            JumpIfTrue = 5,
+            JumpIfFalse = 6,
+            LessThan = 7,
+            Equals = 8,
+            Stop = 99,
+        }
+
+        enum Mode
+        {
+            Position = 0,
+            Immediate = 1
+        }
+    }
+}
