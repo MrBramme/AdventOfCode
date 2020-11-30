7523c2faf2019a727910b45eed13fc97a7fccfc6
Bram Hendrickx <bram_hendrickx@hotmail.com>
2020-11-30 23:38:24 +0100

    Added part 2 of day 1 2019

---
+++ AdventOfCode/AdventOfCode.Year2019.UnitTests/Solutions/Solution201901Part2UnitTest.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2019.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace AdventOfCode.Year2019.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution201901Part2UnitTest
+    {
+        private Solution201901Part2 _sut;
+        private Mock<ILogger<Solution201901Part2>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution201901Part2>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution201901Part2(_loggerMock.Object, _inputService.Object);
+        }
+
+        [TestCase("12", "2")]
+        [TestCase("1969", "966")]
+        [TestCase("100756", "50346")]
+        public void GivenExampleInput_ReturnsExpected(string input, string expected)
+        {
+            // Given
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns(new[] { input });
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+
+        [Test]
+        public void GivenMultipleInput_ReturnsExpected()
+        {
+            // Given
+            var expected = "51314";
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns("12,1969,100756".Split(','));
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Year2019/Solutions/Solution201901Part2.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace AdventOfCode.Year2019.Solutions
+{
+    public class Solution201901Part2 : ISolution
+    {
+        private readonly ILogger<Solution201901Part2> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2019\\Day01.txt";
+
+        public Solution201901Part2(ILogger<Solution201901Part2> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+        public string GetSolution()
+        {
+            var massInput = _inputService.GetInput(resourceLocation);
+            var result = 0;
+            foreach (var input in massInput)
+            {
+                var currentFuel = 0;
+                var mass = decimal.Parse(input);
+                while (mass > 0)
+                {
+                    var fuelRequirement = GetFuelRequirementForMass(mass);
+                    currentFuel += fuelRequirement;
+                    mass = fuelRequirement;
+                }
+
+                result += currentFuel;
+            }
+            return $"{result}";
+        }
+
+        private int GetFuelRequirementForMass(decimal mass)
+        {
+            var fuel = Math.Floor(mass / 3) - 2;
+            if (fuel < 0)
+            {
+                fuel = 0;
+            }
+
+            return (int)fuel;
+        }
+    }
+}
