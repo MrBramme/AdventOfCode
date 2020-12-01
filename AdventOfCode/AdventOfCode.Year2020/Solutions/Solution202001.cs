3b517e1df2a0f4d602bdf4f175ba170a204748cd
Bram Hendrickx <bram_hendrickx@hotmail.com>
2020-12-01 22:07:31 +0100

    Added 2020 day 1

---
+++ AdventOfCode/AdventOfCode.Year2020/Solutions/Solution202001.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace AdventOfCode.Year2020.Solutions
+{
+    public class Solution202001 : ISolution
+    {
+        private readonly ILogger<Solution202001> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2020\\Day01.txt";
+
+        public Solution202001(ILogger<Solution202001> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+        public string GetSolution()
+        {
+            var expenses = _inputService.GetInput(resourceLocation).Select(int.Parse);
+            var result = 0;
+            foreach (var expense in expenses)
+            {
+                if (expenses.Contains(2020 - expense))
+                {
+                    result = expense * (2020 - expense);
+                    break;
+                }
+            }
+            return $"{result}";
+        }
+    }
+}
