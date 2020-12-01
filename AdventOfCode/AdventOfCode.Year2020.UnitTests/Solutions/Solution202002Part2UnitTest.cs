bea75ebb2db1c1093494a53cc5c468d10290076a
Bram Hendrickx <bram_hendrickx@hotmail.com>
2020-12-01 22:53:54 +0100

    Added groundwork for 2020 day 2

---
+++ AdventOfCode/AdventOfCode.Year2020.UnitTests/Solutions/Solution202002Part2UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2020.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace AdventOfCode.Year2020.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202002Part2UnitTest
+    {
+        private Solution202002Part2 _sut;
+        private Mock<ILogger<Solution202002Part2>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202002Part2>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202002Part2(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenExampleInput_ReturnsExpected()
+        {
+            // Given
+            var expected = "514579";
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns("1721,979,366,299,675,1456".Split(','));
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Year2020/Solutions/Solution202002Part2.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace AdventOfCode.Year2020.Solutions
+{
+    public class Solution202002Part2 : ISolution
+    {
+        private readonly ILogger<Solution202002Part2> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2020\\Day02.txt";
+
+        public Solution202002Part2(ILogger<Solution202002Part2> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+        public string GetSolution()
+        {
+            var input = _inputService.GetInput(resourceLocation).Select(int.Parse);
+            var result = 0;
+
+            return $"{result}";
+        }
+    }
+}
