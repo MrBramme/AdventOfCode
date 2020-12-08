8f0926d7edc011e92550f07cb366a556b09a1f4e
Bram Hendrickx <bram_hendrickx@hotmail.com>
2020-12-08 17:48:45 +0100

    added 2020 day 8 part 2

---
+++ AdventOfCode/AdventOfCode.Year2020.UnitTests/Solutions/Solution202008Part2UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2020.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace AdventOfCode.Year2020.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202008Part2UnitTest
+    {
+        private Solution202008Part2 _sut;
+        private Mock<ILogger<Solution202008Part2>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202008Part2>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202008Part2(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenMultiple_ReturnsExpectedSum()
+        {
+            // Given
+            var expected = "8";
+            var input = new[] { "nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6" };
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns(input);
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Year2020/Solutions/Solution202008Part2.cs
@@ -0,0 +1,86 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Year2020.Solutions
+{
+    public class Solution202008Part2 : ISolution
+    {
+        private readonly ILogger<Solution202008Part2> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2020\\Day08.txt";
+
+        public Solution202008Part2(ILogger<Solution202008Part2> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+
+        public string GetSolution()
+        {
+            var inputData = _inputService.GetInput(resourceLocation);
+
+            var result = -1;
+            for (var i = 0; i < inputData.Length; i++)
+            {
+                var index = 0;
+                var value = 0;
+                var pastIndices = new List<int>();
+                var currentValue = inputData[i].Split()[0];
+                if (currentValue.Equals("nop") || currentValue.Equals("jmp"))
+                {
+                    var isDone = false;
+                    var isSuccess = false;
+
+                    var input = new List<string>(inputData).ToArray();
+                    input[i] = currentValue.Equals("nop") ? inputData[i].Replace("nop", "jmp") : inputData[i].Replace("jmp", "nop");
+
+                    while (!isDone)
+                    {
+                        if (index >= input.Length)
+                        {
+                            isSuccess = true;
+                            isDone = true;
+                            result = value;
+                            break;
+                        }
+                        if (pastIndices.Contains(index))
+                        {
+                            isDone = true;
+                            break;
+                        }
+                        pastIndices.Add(index);
+                        var instruction = GetInstruction(input[index]);
+                        switch (instruction.Item1)
+                        {
+                            case "nop":
+                                index++;
+                                break;
+                            case "acc":
+                                index++;
+                                value += instruction.Item2;
+                                break;
+                            case "jmp":
+                                index += instruction.Item2;
+                                break;
+                        }
+                    }
+
+                    if (isSuccess)
+                    {
+                        break;
+                    }
+                }
+            }
+            return $"{result}";
+        }
+
+        private static ValueTuple<string, int> GetInstruction(string input)
+        {
+            var parts = input.Split(' ');
+            var value = int.Parse(parts[1]);
+            return (parts[0], value);
+        }
+    }
+}
