f6dd8813a01254e0a3855eddc121b81c51543ee4
Bram Hendrickx <bram_hendrickx@hotmail.com>
2020-12-08 17:25:57 +0100

    added 2020 day 8 part 1

---
+++ AdventOfCode/AdventOfCode.Year2020.UnitTests/Solutions/Solution202008UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2020.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace AdventOfCode.Year2020.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202008UnitTest
+    {
+        private Solution202008 _sut;
+        private Mock<ILogger<Solution202008>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202008>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202008(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenMultiple_ReturnsExpectedSum()
+        {
+            // Given
+            var expected = "5";
+            var input = new[] { "nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6" };
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns(input);
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Year2020/Solutions/Solution202008.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Year2020.Solutions
+{
+    public class Solution202008 : ISolution
+    {
+        private readonly ILogger<Solution202008> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2020\\Day08.txt";
+
+        public Solution202008(ILogger<Solution202008> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+
+        public string GetSolution()
+        {
+            var inputData = _inputService.GetInput(resourceLocation);
+            var pastIndices = new List<int>();
+            (int index, int value) state = (0, 0);
+            var isDone = false;
+            while (!isDone)
+            {
+                if (pastIndices.Contains(state.index))
+                {
+                    isDone = true;
+                    break;
+                }
+                pastIndices.Add(state.index);
+                var instruction = GetInstruction(inputData[state.index]);
+                switch (instruction.Item1)
+                {
+                    case "nop":
+                        state.index++;
+                        break;
+                    case "acc":
+                        state.index++;
+                        state.value += instruction.Item2;
+                        break;
+                    case "jmp":
+                        state.index += instruction.Item2;
+                        break;
+                }
+            }
+            return $"{state.value}";
+        }
+
+        private static ValueTuple<string, int> GetInstruction(string input)
+        {
+            var parts = input.Split(' ');
+            var value = int.Parse(parts[1]);
+            return (parts[0], value);
+        }
+    }
+}
