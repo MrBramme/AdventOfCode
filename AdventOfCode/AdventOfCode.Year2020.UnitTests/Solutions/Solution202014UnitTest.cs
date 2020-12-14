eb6439047c6aada6f9d1890ce7677950b965b382
Bram Hendrickx <bram_hendrickx@hotmail.com>
2020-12-14 22:16:59 +0100

    2020 day 14

---
+++ AdventOfCode/AdventOfCode.Year2020.UnitTests/Solutions/Solution202014UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2020.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+namespace AdventOfCode.Year2020.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202014UnitTest
+    {
+        private Solution202014 _sut;
+        private Mock<ILogger<Solution202014>> _loggerMock;
+        private Mock<IInputService> _inputService;
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202014>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202014(_loggerMock.Object, _inputService.Object);
+        }
+        [Test]
+        public void GivenExample_ReturnsExpected()
+        {
+            // Given
+            var expected = "165";
+            var input = new[] { "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", "mem[8] = 11", "mem[7] = 101", "mem[8] = 0" };
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns(input);
+            // When
+            var result = _sut.GetSolution();
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Year2020/Solutions/Solution202014.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace AdventOfCode.Year2020.Solutions
+{
+    public class Solution202014 : ISolution
+    {
+        private readonly ILogger<Solution202014> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2020\\Day14.txt";
+        public Solution202014(ILogger<Solution202014> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+        public string GetSolution()
+        {
+            var inputData = _inputService.GetInput(resourceLocation);
+
+            var mem = new Dictionary<string, long>();
+            var currentMask = string.Empty;
+            var valueRegex = new Regex("mem\\[(?'memlocation'\\d*)\\] = (?'value'\\d*)");
+            for (var index = 0; index < inputData.Length; index++)
+            {
+                if (inputData[index].StartsWith("mask"))
+                {
+                    currentMask = inputData[index].Substring(7);
+                }
+                else
+                {
+                    var matches = valueRegex.Match(inputData[index]);
+                    var memLocation = matches.Groups["memlocation"].Value;
+                    var value = long.Parse(matches.Groups["value"].Value);
+                    var valueAsBinary = Convert.ToString(value, 2).PadLeft(36, '0');
+                    var maskedValue = valueAsBinary.Zip(currentMask, (originalBit, maskBit) =>
+                    {
+                        return maskBit switch
+                        {
+                            'X' => originalBit,
+                            _ => maskBit
+                        };
+                    });
+                    mem[memLocation] = Convert.ToInt64(new string(maskedValue.ToArray()), 2);
+                }
+            }
+
+            var result = mem.Values.Sum();
+            return $"{result}";
+        }
+    }
+}
