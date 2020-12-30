7e9a0a80c029cecf2caa11c3d207f82a8a49fa5a
Bram Hendrickx <bram_hendrickx@hotmail.com>
2020-12-30 20:35:43 +0100

    2020 Day 19 Part 2

---
+++ AdventOfCode/AdventOfCode.Year2020.UnitTests/Solutions/Solution202019Part2UnitTest.cs
@@ -0,0 +1,87 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2020.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+namespace AdventOfCode.Year2020.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202019Part2UnitTest
+    {
+        private Solution202019Part2 _sut;
+        private Mock<ILogger<Solution202019Part2>> _loggerMock;
+        private Mock<IInputService> _inputService;
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202019Part2>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202019Part2(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenExample_ReturnsExpected()
+        {
+            // Given
+            var expected = "12";
+            var input = new[]
+            {
+                "42: 9 14 | 10 1",
+                "9: 14 27 | 1 26",
+                "10: 23 14 | 28 1",
+                "1: \"a\"",
+                "11: 42 31",
+                "5: 1 14 | 15 1",
+                "19: 14 1 | 14 14",
+                "12: 24 14 | 19 1",
+                "16: 15 1 | 14 14",
+                "31: 14 17 | 1 13",
+                "6: 14 14 | 1 14",
+                "2: 1 24 | 14 4",
+                "0: 8 11",
+                "13: 14 3 | 1 12",
+                "15: 1 | 14",
+                "17: 14 2 | 1 7",
+                "23: 25 1 | 22 14",
+                "28: 16 1",
+                "4: 1 1",
+                "20: 14 14 | 1 15",
+                "3: 5 14 | 16 1",
+                "27: 1 6 | 14 18",
+                "14: \"b\"",
+                "21: 14 1 | 1 14",
+                "25: 1 1 | 1 14",
+                "22: 14 14",
+                "8: 42",
+                "26: 14 22 | 1 20",
+                "18: 15 15",
+                "7: 14 5 | 1 21",
+                "24: 14 1",
+                "",
+                "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa",
+                "bbabbbbaabaabba",
+                "babbbbaabbbbbabbbbbbaabaaabaaa",
+                "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
+                "bbbbbbbaaaabbbbaaabbabaaa",
+                "bbbababbbbaaaaaaaabbababaaababaabab",
+                "ababaaaaaabaaab",
+                "ababaaaaabbbaba",
+                "baabbaaaabbaaaababbaababb",
+                "abbbbabbbbaaaababbbbbbaaaababb",
+                "aaaaabbaabaaaaababaa",
+                "aaaabbaaaabbaaa",
+                "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
+                "babaaabbbaaabaababbaabababaaab",
+                "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"
+            };
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns(input);
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
