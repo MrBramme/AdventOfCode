5986cfcc79b8eb1955df52290ad5220cc014b0af
Bram Hendrickx <bram_hendrickx@hotmail.com>
2021-12-02 19:43:17 +0100

    2021 - 2 part 1

---
+++ AdventOfCode/AdventOfCode.Year2021.UnitTests/Solutions/Solution202102UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2021.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace AdventOfCode.Year2021.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202102UnitTest
+    {
+        private Solution202102 _sut;
+        private Mock<ILogger<Solution202102>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202102>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202102(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenExampleInput_ReturnsExpected()
+        {
+            // Given
+            var expected = "150";
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns("forward 5,down 5,forward 8,up 3,down 8,forward 2".Split(','));
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Year2021/Solutions/Solution202102.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2021.Solutions
+{
+    public class Solution202102 : ISolution
+    {
+        private readonly ILogger<Solution202102> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2021\\Day02.txt";
+
+        public Solution202102(ILogger<Solution202102> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+
+        public string GetSolution()
+        {
+            var course = _inputService.GetInput(resourceLocation);
+            var position = new Position();
+            return course.Aggregate(position, (acc, step) =>
+            {
+                var stepParts = step.Split(" ");
+                switch (stepParts[0])
+                {
+                    case "forward":
+                        acc.X += int.Parse(stepParts[1]);
+                        break;
+                    case "down":
+                        acc.Depth -= int.Parse(stepParts[1]);
+                        break;
+                    case "up":
+                        acc.Depth += int.Parse(stepParts[1]);
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+                return acc;
+            }).GetResult;
+        }
+
+        private class Position
+        {
+            public int X { get; set; }
+            public int Depth { get; set; }
+            public string GetResult => Math.Abs(X * Depth).ToString();
+        }
+    }
+}
