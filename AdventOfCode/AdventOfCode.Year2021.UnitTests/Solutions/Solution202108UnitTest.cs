a0c26a87b2170cfd74f689e3beb11f4bd2388281
Bram Hendrickx <bram_hendrickx@hotmail.com>
2021-12-08 21:28:20 +0100

    2021 - 8 part 1

---
+++ AdventOfCode/AdventOfCode.Year2021.UnitTests/Solutions/Solution202108UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2021.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace AdventOfCode.Year2021.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202108UnitTest
+    {
+        private Solution202108 _sut;
+        private Mock<ILogger<Solution202108>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202108>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202108(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenExampleInput_ReturnsExpected()
+        {
+            // Given
+            var expected = "26";
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe;edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc;fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg;fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb;aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea;fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb;dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe;bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef;egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb;gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce".Split(';'));
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Year2021/Solutions/Solution202108.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2021.Solutions
+{
+    public class Solution202108 : ISolution
+    {
+        private readonly ILogger<Solution202108> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2021\\Day08.txt";
+
+        public Solution202108(ILogger<Solution202108> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+
+        public string GetSolution()
+        {
+            var patterns = _inputService.GetInput(resourceLocation).Select(p => p.Split(" | ").Select(i => i.Split(" ")));
+            var validLengths = new[] { 2, 3, 4, 7}; // length of segment input for 1 = 2, 7 = 3, 4 = 4, 7 = 8
+            var result = patterns.Sum(p => p.Last().Count(i => validLengths.Contains(i.Length)));
+            return $"{result}";
+        }
+
+
+
+    }
+
+    
+}
