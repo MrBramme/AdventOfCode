fe5dd2330c6e2d5106b047cf3c6888531a9eff77
Bram Hendrickx <bram_hendrickx@hotmail.com>
2021-12-11 20:26:38 +0100

    2021 - 11 parts 1 & 2

---
+++ AdventOfCode/AdventOfCode.Year2021.UnitTests/Solutions/Solution202111Part2UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2021.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace AdventOfCode.Year2021.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202111Part2UnitTest
+    {
+        private Solution202111Part2 _sut;
+        private Mock<ILogger<Solution202111Part2>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202111Part2>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202111Part2(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenExampleInput_ReturnsExpected()
+        {
+            // Given
+            var expected = "195";
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns("5483143223;2745854711;5264556173;6141336146;6357385478;4167524645;2176841721;6882881134;4846848554;5283751526".Split(';'));
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Year2021/Solutions/Solution202111Part2.cs
@@ -0,0 +1,127 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2021.Solutions
+{
+    public class Solution202111Part2 : ISolution
+    {
+        private readonly ILogger<Solution202111Part2> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2021\\Day11.txt";
+
+        public Solution202111Part2(ILogger<Solution202111Part2> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+
+        public string GetSolution()
+        {
+            var grid = _inputService.GetInput(resourceLocation).Select(i => i.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray()).ToArray();
+            var gridWidth = grid[0].Length;
+            var gridHeight = grid.Length;
+            var lastPoint = new Point(gridWidth - 1, gridHeight - 1);
+
+            var octopusses = new List<Octopus>();
+            for(var y = 0; y < gridHeight; y++)
+            {
+                for(var x = 0; x < gridWidth; x++)
+                {
+                    octopusses.Add(new Octopus(new Point(x, y), grid[y][x], lastPoint));
+                }
+            }
+
+            var step = 0;
+            var isDone = false;
+            while(!isDone)
+            {
+                step++;
+                octopusses.ForEach(o => o.StartStep(step));
+                var neighboursToFlash = new List<Point>();
+                octopusses.ForEach(o => neighboursToFlash.AddRange(o.Flash(0)));
+                while (neighboursToFlash.Any())
+                {
+                    var nextCycle = new List<Point>();
+                    foreach(var neighbour in neighboursToFlash)
+                    {
+                        nextCycle.AddRange(octopusses.First(o => neighbour.X == o.Position.X && neighbour.Y == o.Position.Y).Flash(1));
+                    }
+                    neighboursToFlash = nextCycle;
+                }
+                octopusses.ForEach(o => o.FinalStage());
+                isDone = octopusses.Sum(o => o.Energy) == 0;
+            }
+
+            return $"{step}";
+        }
+
+        private class Octopus
+        {
+            public int Energy;
+            public Point Position;
+            private List<Point> _neighbours;
+            public bool IsFlash => Energy > 9 && _lastFlashStep != _stepCount;
+            public int FlashCount = 0;
+            private int _stepCount;
+            private int _lastFlashStep;
+
+            public Octopus(Point position, int energy, Point LastPoint)
+            {
+                Position = position;
+                Energy = energy;
+                _neighbours = GetNeighbours(position, LastPoint);
+            }
+
+            private List<Point> GetNeighbours(Point point, Point LastPoint)
+            {
+                var neighbours = new List<Point>();
+                for(var x = -1; x <= 1; x++)
+                {
+                    for(var y = -1; y <= 1; y++)
+                    {
+                        if (!(x == 0 && y == 0))
+                        {
+                            var neighbour = new Point(point.X + x, point.Y + y);
+                            if (neighbour.X >= 0 && neighbour.Y >= 0 && neighbour.X <= LastPoint.X && neighbour.Y <= LastPoint.Y)
+                            {
+                                neighbours.Add(neighbour);
+                            }
+                        }
+                    }
+                }
+                return neighbours;
+            }
+
+            public void StartStep(int stepCount)
+            {
+                _stepCount = stepCount;
+                Energy++;
+            }
+
+            public List<Point> Flash(int energyIncrease)
+            {
+                Energy += energyIncrease;
+                if (IsFlash)
+                {
+                    _lastFlashStep = _stepCount;
+                    FlashCount++;
+                    return _neighbours;
+                }
+                return new List<Point>(0);
+            }
+
+            public void FinalStage()
+            {
+                if(Energy > 9)
+                {
+                    Energy = 0;
+                }
+            }
+        }
+
+        private record Point (int X, int Y);
+    }
+}
