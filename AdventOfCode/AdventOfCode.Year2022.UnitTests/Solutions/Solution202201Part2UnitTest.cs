387fa3b36bc6e80359ae0d83b05cd3a5d2002fd1
Bram Hendrickx <bram_hendrickx@hotmail.com>
2022-12-01 09:56:01 +0100

    added 2022-01 part 2

---
+++ AdventOfCode/AdventOfCode.Year2022.UnitTests/Solutions/Solution202201Part2UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2022.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace AdventOfCode.Year2022.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202201Part2UnitTest
+    {
+        private Solution202201Part2 _sut;
+        private Mock<ILogger<Solution202201Part2>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202201Part2>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202201Part2(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenExampleInput_ReturnsExpected()
+        {
+            // Given
+            var expected = "45000";
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns("1000,2000,3000,,4000,,5000,6000,,7000,8000,9000,,10000".Split(','));
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
+
---
+++ AdventOfCode/AdventOfCode.Year2022/Solutions/Solution202201Part2.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace AdventOfCode.Year2022.Solutions
+{
+    public class Solution202201Part2 : ISolution
+    {
+        private readonly ILogger<Solution202201Part2> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2022\\Day01.txt";
+
+        public Solution202201Part2(ILogger<Solution202201Part2> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+        public string GetSolution()
+        {
+            var caloriesInput = _inputService.GetInput(resourceLocation).ToList();
+            var caloriesPerElf = new List<int>();
+            var currentCalories = 0;
+
+            foreach (var calories in caloriesInput)
+            {
+                if (string.IsNullOrWhiteSpace(calories))
+                {
+                    caloriesPerElf.Add(currentCalories);
+                    currentCalories = 0;
+                }
+                else
+                {
+                    currentCalories += int.Parse(calories);
+                }
+            }
+            caloriesPerElf.Add(currentCalories);
+            return $"{caloriesPerElf.OrderDescending().Take(3).Sum()}";
+        }
+    }
+}
