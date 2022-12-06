afdcea1f61b748ea432ebe6a0d8893c172988969
Bram Hendrickx <bram_hendrickx@hotmail.com>
2022-12-06 08:29:23 +0100

    Added placeholder 2022 day 6

---
+++ AdventOfCode/AdventOfCode.Year2022.UnitTests/Solutions/Solution202206Part2UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2022.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace AdventOfCode.Year2022.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202206Part2UnitTest
+    {
+        private Solution202206Part2 _sut;
+        private Mock<ILogger<Solution202206Part2>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202206Part2>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202206Part2(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenExampleInput_ReturnsExpected()
+        {
+            // Given
+            var expected = "CMZ";
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns("    [D]    \r\n[N] [C]    \r\n[Z] [M] [P]\r\n 1   2   3 \r\n\r\nmove 1 from 2 to 1\r\nmove 3 from 1 to 3\r\nmove 2 from 2 to 1\r\nmove 1 from 1 to 2".Split("\r\n"));
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
+
---
+++ AdventOfCode/AdventOfCode.Year2022/Solutions/Solution202206.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace AdventOfCode.Year2022.Solutions
+{
+    public class Solution202206 : ISolution
+    {
+        private readonly ILogger<Solution202206> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2022/Day06.txt";
+
+        public Solution202206(ILogger<Solution202206> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+
+        public string GetSolution()
+        {
+            throw new NotImplementedException();
+            // var input = _inputService.GetInput(resourceLocation);
+            // var result = "";
+            // return $"{result}";
+        }
+    }
+}
