d024bdeb479a3542688b64f9a843497c0c8c91ae
Bram Hendrickx <bram_hendrickx@hotmail.com>
2022-12-06 20:08:16 +0100

    Added placeholder 2022 day 7

---
+++ AdventOfCode/AdventOfCode.Year2022/Solutions/Solution202207Part2.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace AdventOfCode.Year2022.Solutions
+{
+    public class Solution202207Part2 : ISolution
+    {
+        private readonly ILogger<Solution202207Part2> _logger;
+        private readonly IInputService _inputService;
+        private readonly string resourceLocation = "Resources2022/Day07.txt";
+
+        public Solution202207Part2(ILogger<Solution202207Part2> logger, IInputService inputService)
+        {
+            _logger = logger;
+            _inputService = inputService;
+        }
+
+        public string GetSolution()
+        {
+            throw new NotImplementedException();
+            var input = _inputService.GetInput(resourceLocation).First().ToCharArray();
+            var result = -1;
+
+            return $"{result}";
+        }
+    }
+}
