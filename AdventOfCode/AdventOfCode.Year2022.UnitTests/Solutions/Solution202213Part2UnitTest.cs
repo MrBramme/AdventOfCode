c06cc6521b92e5cd1894ebb09822323af618501d
MrBramme <MrBramme@users.noreply.github.com>
2022-12-13 20:08:00 +0100

    prep day 12 and 13

---
+++ AdventOfCode/AdventOfCode.Year2022.UnitTests/Solutions/Solution202213Part2UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Domain.Interfaces;
+using AdventOfCode.Year2022.Solutions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace AdventOfCode.Year2022.UnitTests.Solutions
+{
+    [TestFixture]
+    public class Solution202213Part2UnitTest
+    {
+        private Solution202213Part2 _sut;
+        private Mock<ILogger<Solution202213Part2>> _loggerMock;
+        private Mock<IInputService> _inputService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<ILogger<Solution202213Part2>>();
+            _inputService = new Mock<IInputService>();
+            _sut = new Solution202213Part2(_loggerMock.Object, _inputService.Object);
+        }
+
+        [Test]
+        public void GivenExampleInput_ReturnsExpected()
+        {
+            // Given
+            var expected = "13140";
+            _inputService.Setup(x => x.GetInput(It.IsAny<string>())).Returns("addx 15\r\naddx -11\r\naddx 6\r\naddx -3\r\naddx 5\r\naddx -1\r\naddx -8\r\naddx 13\r\naddx 4\r\nnoop\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx -35\r\naddx 1\r\naddx 24\r\naddx -19\r\naddx 1\r\naddx 16\r\naddx -11\r\nnoop\r\nnoop\r\naddx 21\r\naddx -15\r\nnoop\r\nnoop\r\naddx -3\r\naddx 9\r\naddx 1\r\naddx -3\r\naddx 8\r\naddx 1\r\naddx 5\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx -36\r\nnoop\r\naddx 1\r\naddx 7\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\naddx 6\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx 7\r\naddx 1\r\nnoop\r\naddx -13\r\naddx 13\r\naddx 7\r\nnoop\r\naddx 1\r\naddx -33\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\nnoop\r\nnoop\r\nnoop\r\naddx 8\r\nnoop\r\naddx -1\r\naddx 2\r\naddx 1\r\nnoop\r\naddx 17\r\naddx -9\r\naddx 1\r\naddx 1\r\naddx -3\r\naddx 11\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx -13\r\naddx -19\r\naddx 1\r\naddx 3\r\naddx 26\r\naddx -30\r\naddx 12\r\naddx -1\r\naddx 3\r\naddx 1\r\nnoop\r\nnoop\r\nnoop\r\naddx -9\r\naddx 18\r\naddx 1\r\naddx 2\r\nnoop\r\nnoop\r\naddx 9\r\nnoop\r\nnoop\r\nnoop\r\naddx -1\r\naddx 2\r\naddx -37\r\naddx 1\r\naddx 3\r\nnoop\r\naddx 15\r\naddx -21\r\naddx 22\r\naddx -6\r\naddx 1\r\nnoop\r\naddx 2\r\naddx 1\r\nnoop\r\naddx -10\r\nnoop\r\nnoop\r\naddx 20\r\naddx 1\r\naddx 2\r\naddx 2\r\naddx -6\r\naddx -11\r\nnoop\r\nnoop\r\nnoop".Split("\r\n"));
+
+            // When
+            var result = _sut.GetSolution();
+
+            // Then
+            result.Should().Be(expected);
+        }
+    }
+}
+
